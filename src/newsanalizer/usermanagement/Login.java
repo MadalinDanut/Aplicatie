97bd05f7846c43c0f1b16beb8446d107bc5756f2
MadalinDanut <119969424+MadalinDanut@users.noreply.github.com>
2023-02-07 19:30:13 +0200

    Update Login.java

--- src/newsanalizer/usermanagement/Login.java
+++ src/newsanalizer/usermanagement/Login.java
@@ -4,15 +4,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class Login {
 
-    private List<User> listUsers = loadDB();
+    private static List<User> listUsers = loadDB();
 
     static Scanner scanner = new Scanner(System.in);
 
@@ -41,12 +38,11 @@ public class Login {
             userKb.setUsername(kbUsername);
             userKb.setPassword(kbPwd);
             userKb.setAnalyst(Boolean.parseBoolean(kbAnalyst));
-            userKb.setAnalyst(Boolean.parseBoolean(kbAnalyst));
+            userKb.setAdmin(Boolean.parseBoolean(kbAdmin));
 
-            for(User u: listUsers) {
+            for (User u : listUsers) {
 
-                if(u.equalsUsers(userKb))
-                {
+                if (u.equalsUsers(userKb)) {
                     System.out.println("ok, you are logged in now");
                     succes = true;
                 }
@@ -59,67 +55,121 @@ public class Login {
                 TimeUnit.SECONDS.sleep(10);
             }
         }
-        while(!succes);
+        while (!succes);
         // when loggend in print menu based on parameters
+
         printMenu(userKb.isAnalyst(), userKb.isAdmin());
 
     }
 
     static void printMenu(boolean analyst, boolean admin) {
-        if (analyst){
+        if (analyst) {
             System.out.println("print news");
         }
-        if (admin){
+        if (admin) {
             printAdminMeniu();
         }
     }
+    // selectare pentru:
+    // 1- print news
+    // 2 - add user => new method addUser()
+    // 2.1 - addUser() => input for username, pwd, isAnalyst, isAdmin
 
-    static void printAdminMeniu (){
+    public static void printAdminMeniu() {
+        boolean cont = true;
+        int option = -1;
 
-        // selectare pentru:
-        // 1- print news
-        // 2 - add user => new method addUser()
-        // 2.1 - addUser() => input for username, pwd, isAnalyst, isAdmin
+        scanner = new Scanner(System.in);
+        listUsers = new ArrayList<>();
+
+        do {
+            System.out.println("=== Select an Option ===");
+            System.out.println("1. Add a User");
+            System.out.println("2. Show all Users");
+            System.out.println("0. Exit");
+
+            System.out.print("\n  > ");
+
+            try {
+                option = Integer.parseInt(scanner.nextLine());
+
+                System.out.println("\n");
+
+                switch (option) {
+                    case 1:
+                        addUser();
+                        break;
+
+                    case 2:
+                        showUsers();
+                        break;
+
+                    case 0:
+                        System.out.println("Good Bye!");
+                        cont = false;
+                        break;
+
+                    default:
+                        System.err.println("'" + option + "' is not a valid option. Please try again.\n");
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid option selected.\n");
+            }
+
+        } while (cont == true);
     }
-    static void printNews (){
+
+
+    public static void addUser() {
+
+        System.out.println(">> Add User <<\n");
+
+        System.out.print("Enter users name: ");
+        String Name = scanner.nextLine();
+
+        System.out.print("Enter user a password: ");
+        String pwd = scanner.nextLine();
+
+        User addUser = new User();
+        addUser.setUsername(Name);
+        addUser.setPassword(pwd);
+
+        listUsers.add(addUser);
+        System.out.println("\nUser added successfully!\n");
+    }
+
+    public static void showUsers() {
+        System.out.println(">> All Customers <<\n");
+
+
+        for (int i = 0; i < listUsers.size(); i++) {
+            System.out.println(listUsers);
+        }
+
+        System.out.println("\n");
+    }
+
+
+    static void printNews() {
+
+        // Still in progress
 
     }
 
-     void addUser(){
+    private static List<User> loadDB() {
 
-        User username = new User();
-
-        System.out.print("username: ");
-        String kbUsername = scanner.nextLine();
-        System.out.print("pwd: ");
-        String kbPwd = scanner.nextLine();
-
-        System.out.print("is analyst: ");
-        boolean isAnalyst = Boolean.parseBoolean(scanner.nextLine());
-        username.setAnalyst(isAnalyst);
-
-        System.out.print("admin: ");
-        boolean isAdmin = Boolean.parseBoolean(scanner.nextLine());
-        username.setAdmin(isAdmin);
-
-
-
-
-
-    }
-    private List<User> loadDB() {
-
-        Path path= Paths.get("users.txt");
+        Path path = Paths.get("users.txt");
         List<User> listOfUsers = new ArrayList<>();
         try {
-            List<String> listOfUsersAsStrings= Files.readAllLines(path);
+            List<String> listOfUsersAsStrings = Files.readAllLines(path);
             System.out.println(listOfUsersAsStrings);
 
-            for(int i = 0; i<listOfUsersAsStrings.size();i++) {
+            for (int i = 0; i < listOfUsersAsStrings.size(); i++) {
                 User uObj = new User();
                 String currentLineOfText = listOfUsersAsStrings.get(i);
                 StringTokenizer st = new StringTokenizer(currentLineOfText, " ");
-                while(st.hasMoreTokens()) {
+                while (st.hasMoreTokens()) {
                     String u = st.nextToken();
                     String p = st.nextToken();
 
@@ -135,11 +185,10 @@ public class Login {
 
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-        finally {
+        } finally {
         }
 
         return listOfUsers;
-    }
 
+    }
 }
